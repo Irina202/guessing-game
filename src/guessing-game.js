9ca5488997be28b774666ca5d44c239e53700f4a
Irina202 <iracherepanova@gmail.com>
2022-08-13 10:36:38 +1000

    feat: guess number

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,24 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor(start, end) {
+      this.start;
+      this.end;
+    }
+    
     setRange(min, max) {
-
-    }
-
+      this.start = min;
+      this.end = max;
+    } 
+    
     guess() {
-
+      return Math.round((this.end + this.start)/2);
+      
     }
-
-    lower() {
-
-    }
-
+  
+  lower() {
+    this.end = Math.round((this.end + this.start)/2);
+  }
     greater() {
-
+      this.start = Math.round((this.end + this.start)/2);
     }
-}
-
+  }
 module.exports = GuessingGame;
